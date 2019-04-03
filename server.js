725c989fcc4dd4c576c99925d124c07e0ae5b403
paintedlbird7 <rosaperezlarios@gmail.com>
2019-04-03 14:15:28 -0700

    request handler

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ server.use(helmet());
 server.use(morgan('dev'));
 
 
+// server.use(teamNamer);
+// server.use(moodyGateKeeper);
+
+// request handler that responds with a custom message for invalid URLs.
+// if a client visits a non-existing endpoint, they will get a default message provided by their client 
+//when a resource is not found on a server
+server.use((req, res) => {
+  res.status(500).send("Ain't nobody got time for that!")
+})
 
 
 module.exports = server;
